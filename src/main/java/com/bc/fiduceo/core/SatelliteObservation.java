940ad005db89da8d6f5427a4ae6e29dde025505d
tom <tom.block@brockmann-consult.de>
2015-08-03 15:08:13 +0200

    added classes

---
+++ src/main/java/com/bc/fiduceo/core/SatelliteObservation.java
@@ -0,0 +1,47 @@
+package com.bc.fiduceo.core;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import java.util.Date;
+
+public class SatelliteObservation {
+
+    private Date startTime;
+    private Date stopTime;
+
+    private Geometry geometry;
+
+    private Sensor sensor;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getStopTime() {
+        return stopTime;
+    }
+
+    public void setStopTime(Date stopTime) {
+        this.stopTime = stopTime;
+    }
+
+    public Geometry getGeometry() {
+        return geometry;
+    }
+
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+}
---
+++ src/main/java/com/bc/fiduceo/core/Sensor.java
@@ -0,0 +1,14 @@
+package com.bc.fiduceo.core;
+
+public class Sensor {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
