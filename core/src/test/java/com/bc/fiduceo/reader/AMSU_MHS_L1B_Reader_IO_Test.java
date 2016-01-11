4dfd076d2c63fee6b50beb08d8ca1ec00344db30
Muhammad <muhammshu@yahoo.com>
2016-01-11 14:52:00 +0100

    Refactoring

--- core/src/test/java/com/bc/fiduceo/reader/AMSU_MHS_L1B_Reader_IO_Test.java
+++ core/src/test/java/com/bc/fiduceo/reader/AMSU_MHS_L1B_Reader_IO_Test.java
@@ -36,48 +36,41 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * @author muhammad.bc
- */
+
 @RunWith(IOTestRunner.class)
 public class AMSU_MHS_L1B_Reader_IO_Test {
 
-
     private AMSU_MHS_L1B_Reader reader;
-    private File testDataDirectory;
-    private File file;
 
     @Before
     public void setUp() throws IOException {
         reader = new AMSU_MHS_L1B_Reader();
-        testDataDirectory = TestUtil.getTestDataDirectory();
+        File testDataDirectory = TestUtil.getTestDataDirectory();
+
+        File file = new File(testDataDirectory, "NSS.AMBX.NK.D15348.S0057.E0250.B9144748.GC.h5");
+        reader.open(file);
     }
 
-    @Test
-    public void testOpenHDF5() throws IOException {
-        file = new File(testDataDirectory, "NSS.AMBX.NK.D15348.S0057.E0250.B9144748.GC.h5");
-        reader.open(file);
+    @After
+    public void tearDown() throws IOException {
         reader.close();
     }
 
     @Test
     public void testAcqusitionInfo() throws IOException, ParseException {
-        file = new File(testDataDirectory, "NSS.AMBX.NK.D15348.S0057.E0250.B9144748.GC.h5");
-        reader.open(file);
         AcquisitionInfo read = reader.read();
         List<Point> coordinates = read.getCoordinates();
-        Assert.assertNotNull(read.getSensingStop());
-        Assert.assertNotNull(read.getSensingStart());
-        assertNotNull(coordinates);
 
+        assertNotNull(coordinates);
         assertEquals(497, coordinates.size());
         assertCoordinate(-978654.0, 214099.0, coordinates.get(0));
         assertCoordinate(-785613.0, 260409.0, coordinates.get(10));
         assertCoordinate(-978654.0, 214099.0, coordinates.get(coordinates.size() - 1));
 
+        assertNotNull(read.getSensingStart());
+        assertNotNull(read.getSensingStop());
         TestUtil.assertCorrectUTCDate(2015, 12, 13, 23, 15, 30, 128, read.getSensingStart());
         TestUtil.assertCorrectUTCDate(2015, 12, 14, 1, 1, 32, 787, read.getSensingStop());
-
     }
 
     private void assertCoordinate(double expectedX, double expectedY, Point coordinate) {
@@ -86,10 +79,4 @@ public class AMSU_MHS_L1B_Reader_IO_Test {
     }
 
 
-    @After
-    public void testCloseHDF5() throws IOException {
-        file = new File(testDataDirectory, "NSS.AMBX.NK.D15348.S0057.E0250.B9144748.GC.h5");
-        reader.open(file);
-        reader.close();
-    }
 }
