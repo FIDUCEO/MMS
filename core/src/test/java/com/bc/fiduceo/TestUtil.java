998439275076aa0de66a2f453aaa7ef277429f39
Muhammad <muhammshu@yahoo.com>
2016-01-28 17:39:08 +0100

    S2 Polygon, test intersection with polygon from the file.

--- core/src/test/java/com/bc/fiduceo/TestUtil.java
+++ core/src/test/java/com/bc/fiduceo/TestUtil.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2015 Brockmann Consult GmbH
  * This code was developed for the EC project "Fidelity and Uncertainty in
@@ -21,14 +20,24 @@
 
 package com.bc.fiduceo;
 
+import com.bc.fiduceo.core.NodeType;
+import com.bc.fiduceo.geometry.GeometryFactory;
+import com.bc.fiduceo.geometry.Point;
+import com.google.common.geometry.S2LatLng;
+import com.google.common.geometry.S2Point;
+import com.vividsolutions.jts.geom.Coordinate;
 import org.esa.snap.core.datamodel.ProductData;
 import org.esa.snap.core.util.io.FileUtils;
+import ucar.ma2.ArrayDouble;
+import ucar.ma2.ArrayInt;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
@@ -84,7 +93,6 @@ public class TestUtil {
         if (!testDir.mkdirs()) {
             fail("unable to create test directory: " + testDir.getAbsolutePath());
         }
-
         return testDir;
     }
 
@@ -112,4 +120,126 @@ public class TestUtil {
         final String tempDirPath = System.getProperty(SYSTEM_TEMP_PROPETY);
         return new File(tempDirPath, TEST_DIRECTORY);
     }
+
+    public static com.bc.fiduceo.geometry.Polygon allBoundingPolygon(ArrayDouble.D2 arrayLatitude, ArrayDouble.D2 arrayLongitude, NodeType nodeType, GeometryFactory.Type type) {
+        final int[] shape = arrayLatitude.getShape();
+        int width = shape[1] - 1;
+        int height = shape[0] - 1;
+
+        int intervalX = 50;
+        int intervalY = 50;
+
+        com.bc.fiduceo.geometry.GeometryFactory geometryFactory = new com.bc.fiduceo.geometry.GeometryFactory(type);
+        List<Point> coordinates = new ArrayList<>();
+
+        int[] timeAxisStart = new int[2];
+        int[] timeAxisEnd = new int[2];
+        if (nodeType == NodeType.ASCENDING) {
+            for (int x = 0; x < width; x += intervalX) {
+                final double lon = arrayLongitude.get(0, x);
+                final double lat = arrayLatitude.get(0, x);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+
+            timeAxisStart[0] = coordinates.size();
+            timeAxisEnd[0] = timeAxisStart[0];
+            for (int y = 0; y < height; y += intervalY) {
+                final double lon = arrayLongitude.get(y, width);
+                final double lat = arrayLatitude.get(y, width);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+                ++timeAxisEnd[0];
+            }
+
+            for (int x = width; x > 0; x -= intervalX) {
+                final double lon = arrayLongitude.get(height, x);
+                final double lat = arrayLatitude.get(height, x);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+
+            for (int y = height; y > 0; y -= intervalY) {
+                final double lon = arrayLongitude.get(y, 0);
+                final double lat = arrayLatitude.get(y, 0);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+        } else {
+            timeAxisStart[0] = 0;
+            timeAxisEnd[0] = 0;
+            for (int y = 0; y < height; y += intervalY) {
+                final double lon = arrayLongitude.get(y, width);
+                final double lat = arrayLatitude.get(y, width);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+                ++timeAxisEnd[0];
+            }
+
+            for (int x = width; x > 0; x -= intervalX) {
+                final double lon = arrayLongitude.get(height, x);
+                final double lat = arrayLatitude.get(height, x);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+
+            for (int y = height; y > 0; y -= intervalY) {
+                final double lon = arrayLongitude.get(y, 0);
+                final double lat = arrayLatitude.get(y, 0);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+
+            for (int x = 0; x < width; x += intervalX) {
+                final double lon = arrayLongitude.get(0, x);
+                final double lat = arrayLatitude.get(0, x);
+                coordinates.add(geometryFactory.createPoint(lon, lat));
+            }
+        }
+        if (GeometryFactory.Type.JTS == type) {
+            coordinates.add(coordinates.get(0));
+        }
+        return geometryFactory.createPolygon(coordinates);
+    }
+
+    public static Coordinate[] getCoordinates(List<Point> points) {
+        final Coordinate[] coordinates = new Coordinate[points.size()];
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+            coordinates[i] = new Coordinate(point.getLon(), point.getLat());
+        }
+        return coordinates;
+    }
+
+    public static S2Point createS2Point(double lon, double lat) {
+        return S2LatLng.fromDegrees(lat, lon).toPoint();
+    }
+
+    public static String plotPolygon(Point[] points) {
+        final StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("POLYGON((");
+
+        for (int i = 0; i < points.length; i++) {
+            Point coordinate = points[i];
+            stringBuffer.append(coordinate.getLon());
+            stringBuffer.append(" ");
+            stringBuffer.append(coordinate.getLat());
+            if (i < points.length - 1) {
+                stringBuffer.append(",");
+            }
+        }
+        stringBuffer.append(",");
+        stringBuffer.append(points[0].getLon());
+        stringBuffer.append(" ");
+        stringBuffer.append(points[0].getLat());
+        stringBuffer.append("))");
+
+        System.out.println(stringBuffer.toString());
+        return stringBuffer.toString();
+    }
+
+    public static ArrayDouble.D2 rescaleCoordinate(ArrayInt.D2 coodinate, double scale) {
+        int[] coordinates = (int[]) coodinate.copyTo1DJavaArray();
+        int[] shape = coodinate.getShape();
+        ArrayDouble arrayDouble = new ArrayDouble(shape);
+
+        for (int i = 0; i < coordinates.length; i++) {
+            arrayDouble.setDouble(i, ((coordinates[i] * scale)));
+        }
+        return (ArrayDouble.D2) arrayDouble.copy();
+    }
+
 }
--- core/src/test/java/com/bc/fiduceo/geometry/s2/S2PolygonTest.java
+++ core/src/test/java/com/bc/fiduceo/geometry/s2/S2PolygonTest.java
@@ -1,10 +1,25 @@
 package com.bc.fiduceo.geometry.s2;
 
+import com.bc.fiduceo.TestUtil;
+import com.bc.fiduceo.core.NodeType;
 import com.bc.fiduceo.geometry.Geometry;
+import com.bc.fiduceo.geometry.GeometryFactory;
 import com.bc.fiduceo.geometry.Point;
+import com.bc.fiduceo.geometry.Polygon;
+import com.bc.fiduceo.reader.AMSU_MHS_L1B_Reader;
 import com.bc.geometry.s2.S2WKTReader;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayDouble;
+import ucar.ma2.ArrayInt;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.Variable;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -12,15 +27,29 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by tom.bc on 1/27/2016.
+ * @tom.bc
  */
 public class S2PolygonTest {
 
     private S2WKTReader s2WKTReader;
+    private NetcdfFile netcdfFile;
+    private AMSU_MHS_L1B_Reader reader;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         s2WKTReader = new S2WKTReader();
+
+        File testDataDirectory = TestUtil.getTestDataDirectory();
+        File file = new File(testDataDirectory, "NSS.AMBX.NK.D15348.S0057.E0250.B9144748.GC.h5");
+        netcdfFile = NetcdfFile.open(file.getPath());
+        reader = new AMSU_MHS_L1B_Reader();
+        reader.open(file);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        reader.close();
+        netcdfFile.close();
     }
 
     @Test
@@ -44,19 +73,66 @@ public class S2PolygonTest {
     @Test
     public void testIntersect_intersectionWest() {
         final S2Polygon s2Polygon_1 = createS2Polygon("POLYGON((-5 0, -5 1, -4 1, -4 0, -5 0))");
-        final S2Polygon s2Polygon_2 =  createS2Polygon("POLYGON((-5.5 0, -5.5 1, -4.5 1, -4.5 0, -5.5 0))");
+        final S2Polygon s2Polygon_2 = createS2Polygon("POLYGON((-5.5 0, -5.5 1, -4.5 1, -4.5 0, -5.5 0))");
 
         Geometry intersection = s2Polygon_1.intersection(s2Polygon_2);
         assertNotNull(intersection);
         assertFalse(intersection.isEmpty());
 
         Point[] coordinates = intersection.getCoordinates();
-        assertEquals(5, coordinates.length);
-        assertEquals(-4.5, coordinates[0].getLon(), 1e-8);
-        assertEquals(1.0, coordinates[0].getLat(), 1e-8);
+        assertEquals(7, coordinates.length);
+        assertEquals(-4.0, coordinates[0].getLon(), 1e-8);
+        assertEquals(0.0, coordinates[0].getLat(), 1e-8);
 
-        assertEquals(-5.0, coordinates[2].getLon(), 1e-8);
-        assertEquals(1.0, coordinates[2].getLat(), 1e-8);
+        assertEquals(-4.7499999999998925, coordinates[2].getLon(), 1e-8);
+        assertEquals(1.0000285529444368, coordinates[2].getLat(), 1e-8);
+    }
+
+    @Test
+    public void testIntersact_intersactionNorth() {
+        final S2Polygon s2Polygon_1 = createS2Polygon("POLYGON((-10 -10,-10 10,10 10,10 -10,-10 -10))");
+        final S2Polygon s2Polygon_2 = createS2Polygon("POLYGON((-8 -10,-8 12,9 12,9 -10,-8 -10))");
+
+        Geometry intersection = s2Polygon_1.intersection(s2Polygon_2);
+        assertNotNull(intersection);
+        assertFalse(intersection.isEmpty());
+        Point[] coordinates = intersection.getCoordinates();
+        assertEquals(-9.999999999999998, coordinates[0].getLon(), 1e-8);
+        assertEquals(10.0, coordinates[0].getLat(), 1e-8);
+        assertEquals(9.999999999999998, coordinates[2].getLon(), 1e-8);
+        assertEquals(-10.0, coordinates[2].getLat(), 1e-8);
+
+    }
+
+    @Test
+    public void testIntersact_intersactionSouth() {
+        final S2Polygon s2Polygon_1 = createS2Polygon("POLYGON((-10 -10,-10 10,10 10,10 -10,-10 -10))");
+        final S2Polygon s2Polygon_2 = createS2Polygon("POLYGON((-8 -12,-8 10,9 10,9 -12,-8 -12))");
+
+        Geometry intersection = s2Polygon_1.intersection(s2Polygon_2);
+        assertNotNull(intersection);
+        assertFalse(intersection.isEmpty());
+    }
+
+    @Test
+    public void testIntersact_intersactionEast() {
+        final S2Polygon s2Polygon_1 = createS2Polygon("POLYGON((-10 -10,-10 10,10 10,10 -10,-10 -10))");
+        final S2Polygon s2Polygon_2 = createS2Polygon("POLYGON((-10 -8,12 -8,12 9,-10 9,-10 -8))");
+
+        Geometry intersection = s2Polygon_1.intersection(s2Polygon_2);
+        assertNotNull(intersection);
+        assertFalse(intersection.isEmpty());
+    }
+
+    @Test
+    public void testSamePolygon() {
+        final S2Polygon s2Polygon_1 = createS2Polygon("POLYGON((-10 -10,-10 10,10 10,10 -10,-10 -10))");
+        final S2Polygon s2Polygon_2 = createS2Polygon("POLYGON((10 10,-10 10,-10 -10,10 -10,10 10))");
+
+        Geometry intersection = s2Polygon_1.intersection(s2Polygon_2);
+        assertNotNull(intersection);
+        // This is do to the egdes are the same
+        assertEquals(intersection.toString(), "Polygon: (0) loops:\n");
     }
 
     @Test
@@ -71,6 +147,55 @@ public class S2PolygonTest {
 
         assertEquals(4.0, coordinates[2].getLon(), 1e-8);
         assertEquals(0.0, coordinates[2].getLat(), 1e-8);
+    }
+
+    @Test
+    public void testPolygonIntersectFromFile() throws IOException {
+        Polygon filePolygon = getPolygonFromFile();
+//        com.bc.fiduceo.geometry.Polygon filePolygon = getPolygonFromFile();
+        String s1 = TestUtil.plotPolygon(filePolygon.getCoordinates());
+        assertNotNull(filePolygon);
+
+        Geometry simplePolygon = createS2Polygon("POLYGON ((-100 50, 0 50, 90 50, 90 -50, 0 -50, -100 -50, -100 50))");
+        assertNotNull(simplePolygon);
+
+        final Geometry intersection = simplePolygon.intersection(filePolygon);
+        assertNotNull(intersection);
+        Point[] coordinates = intersection.getCoordinates();
+        assertEquals(141.0140009848692, coordinates[0].getLon(), 1e-8);
+        assertEquals(-69.69470278654627, coordinates[0].getLat(), 1e-8);
+
+        assertEquals(86.49586285175872, coordinates[49].getLon(), 1e-8);
+        assertEquals(-51.6150312821608, coordinates[49].getLat(), 1e-8);
+
+        assertEquals(-68.4959028168305, coordinates[99].getLat(), 1e-8);
+        assertEquals(148.82660078750632, coordinates[99].getLon(), 1e-8);
+
+
+        com.bc.fiduceo.geometry.GeometryFactory factoryS2 = new GeometryFactory(GeometryFactory.Type.S2);
+        String s = TestUtil.plotPolygon(coordinates);
+
+    }
+
+    private Polygon getPolygonFromFile() throws IOException {
+        Array latitude = null;
+        Array longitude = null;
+        float latScale = 1;
+        float longScale = 1;
+        List<Variable> geolocation = netcdfFile.findGroup("Geolocation").getVariables();
+        for (Variable geo : geolocation) {
+            if (geo.getShortName().equals("Latitude")) {
+                latitude = geo.read();
+                latScale = (float) geo.findAttribute("Scale").getNumericValue();
+            } else if (geo.getShortName().equals("Longitude")) {
+                longitude = geo.read();
+                longScale = (float) geo.findAttribute("Scale").getNumericValue();
+            }
+        }
+        ArrayDouble.D2 arrayLong = TestUtil.rescaleCoordinate((ArrayInt.D2) longitude, longScale);
+        ArrayDouble.D2 arrayLat = TestUtil.rescaleCoordinate((ArrayInt.D2) latitude, latScale);
+
+        return TestUtil.allBoundingPolygon(arrayLong, arrayLat, NodeType.ASCENDING, GeometryFactory.Type.S2);
     }
 
     private S2Polygon createS2Polygon(String wellKnownText) {
