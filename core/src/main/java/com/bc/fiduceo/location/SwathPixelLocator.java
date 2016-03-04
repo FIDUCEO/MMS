d20f24bfef8b724723431536055e28854409c5d4
sabine <sabine.embacher@brockmann-consult.de>
2016-03-04 14:42:32 +0100

    implement SnapAvoidCodeDuplicationClass and remove the existing code duplications

--- core/src/main/java/com/bc/fiduceo/location/SwathPixelLocator.java
+++ core/src/main/java/com/bc/fiduceo/location/SwathPixelLocator.java
@@ -1,159 +1,162 @@
 package com.bc.fiduceo.location;
 
-import org.esa.snap.core.datamodel.GeoCoding;
-import org.esa.snap.core.datamodel.GeoPos;
-import org.esa.snap.core.datamodel.PixelPos;
-import org.esa.snap.core.datamodel.ProductData;
-import org.esa.snap.core.datamodel.TiePointGeoCoding;
-import org.esa.snap.core.datamodel.TiePointGrid;
-import org.esa.snap.core.util.ImageUtils;
+//import org.esa.snap.core.datamodel.GeoCoding;
+//import org.esa.snap.core.datamodel.GeoPos;
+//import org.esa.snap.core.datamodel.PixelPos;
+//import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.datamodel.SnapAvoidCodeDuplicationClass_SwathPixelLocator;
+//import org.esa.snap.core.datamodel.TiePointGeoCoding;
+//import org.esa.snap.core.datamodel.TiePointGrid;
+//import org.esa.snap.core.util.ImageUtils;
 import ucar.ma2.Array;
 
-import javax.media.jai.PlanarImage;
-import java.awt.geom.Point2D;
-import java.awt.image.RenderedImage;
-import java.util.ArrayList;
+//import javax.media.jai.PlanarImage;
+//import java.awt.geom.Point2D;
+//import java.awt.image.RenderedImage;
+//import java.util.ArrayList;
 
-public class SwathPixelLocator implements PixelLocator {
+public class SwathPixelLocator extends SnapAvoidCodeDuplicationClass_SwathPixelLocator {
+//public class SwathPixelLocator implements PixelLocator {
 
-    private final GeoPos internalUseGeoPos;
-    private final GeoCoding gc;
-    private final int width;
-    private final int height;
-    private final Point2D.Double internalUsePoint;
-    private BestApproximations bestApproximations;
-    private PixelPos internalUsePixelPos;
+//    private final GeoPos internalUseGeoPos;
+//    private final GeoCoding gc;
+//    private final int width;
+//    private final int height;
+//    private final Point2D.Double internalUsePoint;
+//    private BestApproximations bestApproximations;
+//    private PixelPos internalUsePixelPos;
 
 
     public SwathPixelLocator(Array lonArray, Array latArray, int width, int height) {
-        this.width = width;
-        this.height = height;
-        PlanarImage lonImg = getPlanarImage(lonArray, width, height);
-        PlanarImage latImg = getPlanarImage(latArray, width, height);
-        GeoApproximation[] approximations = GeoApproximation.createApproximations(lonImg, latImg, null, 0.1);
-        bestApproximations = new BestApproximations(approximations);
-        final float[] lats = (float[]) latArray.getStorage();
-        final float[] lons = (float[]) lonArray.getStorage();
-        final TiePointGrid latGrid = new TiePointGrid("lat", width, height, 0.5, 0.5, 1.0, 1.0, lats);
-        final TiePointGrid lonGrid = new TiePointGrid("lon", width, height, 0.5, 0.5, 1.0, 1.0, lons);
-        gc = new TiePointGeoCoding(latGrid, lonGrid);
-        internalUseGeoPos = new GeoPos();
-        internalUsePixelPos = new PixelPos();
-        internalUsePoint = new Point2D.Double();
+        super(lonArray, latArray, width, height);
+//        this.width = width;
+//        this.height = height;
+//        PlanarImage lonImg = getPlanarImage(lonArray, width, height);
+//        PlanarImage latImg = getPlanarImage(latArray, width, height);
+//        GeoApproximation[] approximations = GeoApproximation.createApproximations(lonImg, latImg, null, 0.1);
+//        bestApproximations = new BestApproximations(approximations);
+//        final float[] lats = (float[]) latArray.getStorage();
+//        final float[] lons = (float[]) lonArray.getStorage();
+//        final TiePointGrid latGrid = new TiePointGrid("lat", width, height, 0.5, 0.5, 1.0, 1.0, lats);
+//        final TiePointGrid lonGrid = new TiePointGrid("lon", width, height, 0.5, 0.5, 1.0, 1.0, lons);
+//        gc = new TiePointGeoCoding(latGrid, lonGrid);
+//        internalUseGeoPos = new GeoPos();
+//        internalUsePixelPos = new PixelPos();
+//        internalUsePoint = new Point2D.Double();
     }
 
-    private static PlanarImage getPlanarImage(Array data, int width, int height) {
-        final float[] floats = (float[]) data.getStorage();
-        final ProductData productData = ProductData.createInstance(floats);
-        final RenderedImage lonImage = ImageUtils.createRenderedImage(width, height, productData);
-        return PlanarImage.wrapRenderedImage(lonImage);
-    }
+//    private static PlanarImage getPlanarImage(Array data, int width, int height) {
+//        final float[] floats = (float[]) data.getStorage();
+//        final ProductData productData = ProductData.createInstance(floats);
+//        final RenderedImage lonImage = ImageUtils.createRenderedImage(width, height, productData);
+//        return PlanarImage.wrapRenderedImage(lonImage);
+//    }
 
-    @Override
-    public Point2D getGeoLocation(double x, double y, Point2D g) {
-        internalUsePixelPos.setLocation(x, y);
-        internalUseGeoPos.setInvalid();
-        gc.getGeoPos(internalUsePixelPos, internalUseGeoPos);
-        if (internalUseGeoPos.isValid()) {
-            if (g == null) {
-                g = new Point2D.Double();
-            }
-            g.setLocation(internalUseGeoPos.getLon(), internalUseGeoPos.getLat());
-            return g;
-        }
-        return null;
-    }
+//    @Override
+//    public Point2D getGeoLocation(double x, double y, Point2D g) {
+//        internalUsePixelPos.setLocation(x, y);
+//        internalUseGeoPos.setInvalid();
+//        gc.getGeoPos(internalUsePixelPos, internalUseGeoPos);
+//        if (internalUseGeoPos.isValid()) {
+//            if (g == null) {
+//                g = new Point2D.Double();
+//            }
+//            g.setLocation(internalUseGeoPos.getLon(), internalUseGeoPos.getLat());
+//            return g;
+//        }
+//        return null;
+//    }
 
-    @Override
-    public Point2D[] getPixelLocation(double lon, double lat) {
-        bestApproximations.findFor(lon, lat);
-        if (!bestApproximations.hasApproximations()) {
-            return null;
-        }
-        final ArrayList<Point2D> pipos = new ArrayList<>();
-        final ArrayList<GeoApproximation> theBest = bestApproximations.getTheBest();
-        for (GeoApproximation a : theBest) {
-            internalUsePoint.setLocation(lon, lat);
-            a.g2p(internalUsePoint);
-            if (isValid(internalUsePoint)) {
-                pipos.add(new Point2D.Double(internalUsePoint.getX(), internalUsePoint.getY()));
-            }
-        }
-        return pipos.toArray(new Point2D[0]);
-    }
+//    @Override
+//    public Point2D[] getPixelLocation(double lon, double lat) {
+//        bestApproximations.findFor(lon, lat);
+//        if (!bestApproximations.hasApproximations()) {
+//            return null;
+//        }
+//        final ArrayList<Point2D> pipos = new ArrayList<>();
+//        final ArrayList<GeoApproximation> theBest = bestApproximations.getTheBest();
+//        for (GeoApproximation a : theBest) {
+//            internalUsePoint.setLocation(lon, lat);
+//            a.g2p(internalUsePoint);
+//            if (isValid(internalUsePoint)) {
+//                pipos.add(new Point2D.Double(internalUsePoint.getX(), internalUsePoint.getY()));
+//            }
+//        }
+//        return pipos.toArray(new Point2D[0]);
+//    }
+//
+//    private boolean isValid(Point2D p) {
+//        final int iX = (int) Math.floor(p.getX());
+//        final int iY = (int) Math.floor(p.getY());
+//        return iX >= 0 && iX < width && iY >= 0 && iY < height;
+//    }
 
-    private boolean isValid(Point2D p) {
-        final int iX = (int) Math.floor(p.getX());
-        final int iY = (int) Math.floor(p.getY());
-        return iX >= 0 && iX < width && iY >= 0 && iY < height;
-    }
-
-    private static class BestApproximations {
-
-        private final GeoApproximation[] approximations;
-        private final ArrayList<GeoApproximation> best;
-        private final ArrayList<Integer> bestIdx;
-        private final ArrayList<Double> distances;
-
-        public BestApproximations(GeoApproximation[] approximations) {
-            this.approximations = approximations;
-            best = new ArrayList<>();
-            bestIdx = new ArrayList<>();
-            distances = new ArrayList<>();
-        }
-
-        public void findFor(double lon, double lat) {
-            emptyFields();
-            for (int i = 0; i < approximations.length; i++) {
-                GeoApproximation a = approximations[i];
-                final double distance = a.getDistance(lat, lon);
-                if (distance <= a.getMaxDistance()) {
-                    best.add(a);
-                    bestIdx.add(i);
-                    distances.add(distance);
-                }
-            }
-            findCouplesAndKeepTheBest();
-        }
-
-        private void findCouplesAndKeepTheBest() {
-            for (int i = 1; i < best.size(); i++) {
-                final int first = i - 1;
-                final Integer i1 = bestIdx.get(first);
-                final Integer i2 = bestIdx.get(i);
-                final boolean isNeighbor = i2 - i1 == 1;
-                if (isNeighbor) {
-                    final double d1 = distances.get(first);
-                    final double d2 = distances.get(i);
-                    if (d1 < d2) {
-                        remove(i);
-                    } else {
-                        remove(first);
-                    }
-                    i--;
-                }
-            }
-        }
-
-        private void remove(int i) {
-            best.remove(i);
-            bestIdx.remove(i);
-            distances.remove(i);
-        }
-
-        private void emptyFields() {
-            best.clear();
-            bestIdx.clear();
-            distances.clear();
-        }
-
-        public boolean hasApproximations() {
-            return best.size() > 0;
-        }
-
-        public ArrayList<GeoApproximation> getTheBest() {
-            return best;
-        }
-    }
-
+//    private static class BestApproximations {
+//
+//        private final GeoApproximation[] approximations;
+//        private final ArrayList<GeoApproximation> best;
+//        private final ArrayList<Integer> bestIdx;
+//        private final ArrayList<Double> distances;
+//
+//        public BestApproximations(GeoApproximation[] approximations) {
+//            this.approximations = approximations;
+//            best = new ArrayList<>();
+//            bestIdx = new ArrayList<>();
+//            distances = new ArrayList<>();
+//        }
+//
+//        public void findFor(double lon, double lat) {
+//            emptyFields();
+//            for (int i = 0; i < approximations.length; i++) {
+//                GeoApproximation a = approximations[i];
+//                final double distance = a.getDistance(lat, lon);
+//                if (distance <= a.getMaxDistance()) {
+//                    best.add(a);
+//                    bestIdx.add(i);
+//                    distances.add(distance);
+//                }
+//            }
+//            findCouplesAndKeepTheBest();
+//        }
+//
+//        private void findCouplesAndKeepTheBest() {
+//            for (int i = 1; i < best.size(); i++) {
+//                final int first = i - 1;
+//                final Integer i1 = bestIdx.get(first);
+//                final Integer i2 = bestIdx.get(i);
+//                final boolean isNeighbor = i2 - i1 == 1;
+//                if (isNeighbor) {
+//                    final double d1 = distances.get(first);
+//                    final double d2 = distances.get(i);
+//                    if (d1 < d2) {
+//                        remove(i);
+//                    } else {
+//                        remove(first);
+//                    }
+//                    i--;
+//                }
+//            }
+//        }
+//
+//        private void remove(int i) {
+//            best.remove(i);
+//            bestIdx.remove(i);
+//            distances.remove(i);
+//        }
+//
+//        private void emptyFields() {
+//            best.clear();
+//            bestIdx.clear();
+//            distances.clear();
+//        }
+//
+//        public boolean hasApproximations() {
+//            return best.size() > 0;
+//        }
+//
+//        public ArrayList<GeoApproximation> getTheBest() {
+//            return best;
+//        }
+//    }
+//
 }
