93f3953208b2962eb91bdf7a0b92722a7f3dec19
tom <tom.block@brockmann-consult.de>
2016-03-21 08:58:19 +0100

    added bat/sh, extended assembly

---
+++ matchup-tool/src/main/bin/matchup-tool.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+:: -======================-
+:: User configurable values
+:: -======================-
+
+SET INSTALLDIR=%~dp0%
+
+::------------------------------------------------------------------
+:: You can adjust the Java minimum and maximum heap space here.
+:: Just change the Xms and Xmx options. Space is given in megabyte.
+::    '-Xms64M' sets the minimum heap space to 64 megabytes
+::    '-Xmx512M' sets the maximum heap space to 512 megabytes
+::------------------------------------------------------------------
+SET JAVA_OPTS=-Xms64M -Xmx4096M
+
+
+:: -======================-
+:: Other values
+:: -======================-
+
+SET JAVAEXE=java.exe
+SET LIBDIR=%INSTALLDIR%\lib
+SET OLD_CLASSPATH=%CLASSPATH%
+
+SET CLASSPATH=%INSTALLDIR%\*;%INSTALLDIR%
+
+CALL "%JAVAEXE%" %JAVA_OPTS% -classpath "%CLASSPATH%" com.bc.fiduceo.matchup.MatchupToolMain %*
+
+SET CLASSPATH=%OLD_CLASSPATH%
