1313e57079e9c9290d9dedb98d57f4ff3cf519b7
tom <tom.block@brockmann-consult.de>
2016-04-04 14:29:18 +0200

    corrected script

--- build_and_install_on_cems.sh
+++ build_and_install_on_cems.sh
@@ -1,3 +1,5 @@
 git pull github master
 mvn clean install package assembly:directory
-cp target/fiduceo-master-1.0-SNAPSHOT-MMS/* /group_workspace/cems2/fiduceo/Software/mms/bin
+
+rm -rf /group_workspaces/cems2/fiduceo/Software/mms/bin/*
+cp target/fiduceo-master-1.0-SNAPSHOT-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
