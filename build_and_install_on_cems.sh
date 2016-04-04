ecfdb464281247d6ed395822c29067e3a3e55319
tom <tom.block@brockmann-consult.de>
2016-04-04 14:37:44 +0200

    added recursive to copy

--- build_and_install_on_cems.sh
+++ build_and_install_on_cems.sh
@@ -2,4 +2,4 @@ git pull github master
 mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/fiduceo/Software/mms/bin/*
-cp target/fiduceo-master-1.0-SNAPSHOT-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
+cp -r target/fiduceo-master-1.0-SNAPSHOT-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
