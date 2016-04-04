e4d28ca367ef25ed71127e24428fc38f2fcedeea
sabine <sabine.embacher@brockmann-consult.de>
2016-04-04 14:27:54 +0200

    remove instance check in TimeCondition and ensure that the SampleCollector always collects SampleSets with a primary and a secondary Sample

--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/SampleCollectorTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/SampleCollectorTest.java
@@ -1,32 +1,31 @@
 package com.bc.fiduceo.matchup;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 import com.bc.fiduceo.geometry.GeometryFactory;
 import com.bc.fiduceo.geometry.Point;
 import com.bc.fiduceo.geometry.Polygon;
 import com.bc.fiduceo.location.PixelLocator;
 import com.bc.fiduceo.reader.TimeLocator;
 import com.bc.fiduceo.tool.ToolContext;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.mockito.Mockito;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 public class SampleCollectorTest {
 
     private SampleCollector collector;
     private GeometryFactory factory;
+    private ToolContext context;
 
     @Before
     public void setUp() throws Exception {
         factory = new GeometryFactory(GeometryFactory.Type.S2);
-        final ToolContext context = new ToolContext();
+        context = new ToolContext();
         context.setGeometryFactory(factory);
 
         final PixelLocator pixelLocator = new OffsetPixelLocator(11, 13);
@@ -37,11 +36,11 @@ public class SampleCollectorTest {
     public void testAddPrimarySamples() {
         // preparation
         final List<Point> polygonPoints = createPoints(new double[]{
-                1, 1,
-                3, 1,
-                3, 3,
-                1, 3,
-                1, 1
+                    1, 1,
+                    3, 1,
+                    3, 3,
+                    1, 3,
+                    1, 1
         });
         final Polygon polygon = factory.createPolygon(polygonPoints);
 
@@ -53,10 +52,10 @@ public class SampleCollectorTest {
         final List<SampleSet> sampleSets = matchupSet.getSampleSets();
         assertNotNull(sampleSets);
         final Sample[] expecteds = {
-                new Sample(12, 14, 1.5, 1.5, 14012L),
-                new Sample(13, 14, 2.5, 1.5, 14013L),
-                new Sample(12, 15, 1.5, 2.5, 15012L),
-                new Sample(13, 15, 2.5, 2.5, 15013L)
+                    new Sample(12, 14, 1.5, 1.5, 14012L),
+                    new Sample(13, 14, 2.5, 1.5, 14013L),
+                    new Sample(12, 15, 1.5, 2.5, 15012L),
+                    new Sample(13, 15, 2.5, 2.5, 15013L)
 
         };
         assertEquals(expecteds.length, sampleSets.size());
@@ -78,7 +77,7 @@ public class SampleCollectorTest {
     public void testAddSecondarySamples() {
         final MatchupSet matchupSet = new MatchupSet();
         matchupSet.addPrimary(new Sample(2, 3, 4.5, 5.5, 100L));
-        matchupSet.addPrimary(new Sample(6, 7, 8.5, 9.5, 100L));
+        matchupSet.addPrimary(new Sample(6, 7, 8.5, 9.5, 200L));
 
         collector.addSecondarySamples(matchupSet.getSampleSets(), new TestTimeLocator());
 
@@ -104,13 +103,47 @@ public class SampleCollectorTest {
     }
 
     @Test
+    public void testRemoveSampleSetIfNoSecondarySampleCanBeFound() throws Exception {
+        final PixelLocator locator = Mockito.mock(PixelLocator.class);
+        collector = new SampleCollector(context, locator);
+
+        final Sample primaryOne = new Sample(2, 3, 4.5, 5.5, 100L);
+        final Sample primaryTwo = new Sample(6, 7, 8.5, 9.5, 200L);
+
+        final Point2D[] emptyArray = {};
+        when(locator.getPixelLocation(primaryOne.lon, primaryOne.lat)).thenReturn(emptyArray);
+        when(locator.getPixelLocation(primaryTwo.lon, primaryTwo.lat)).thenReturn(new Point2D[]{new Point2D.Double(12.3, 14.8)});
+        when(locator.getGeoLocation(eq(12.5), eq(14.5), isA(Point2D.class))).thenReturn(new Point2D.Double(170.123, 34.321));
+
+        final MatchupSet matchupSet = new MatchupSet();
+        matchupSet.addPrimary(primaryOne);
+        matchupSet.addPrimary(primaryTwo);
+
+        collector.addSecondarySamples(matchupSet.getSampleSets(), new TestTimeLocator());
+
+        final List<SampleSet> resultSampleSets = matchupSet.getSampleSets();
+        assertEquals(1, resultSampleSets.size());
+
+        SampleSet sampleSet = resultSampleSets.get(0);
+        Sample primary = sampleSet.getPrimary();
+        assertSame(primaryTwo, primary);
+        final Sample secondary = sampleSet.getSecondary();
+        assertNotNull(secondary);
+        assertEquals(34.321, secondary.lat, 1e-8);
+        assertEquals(170.123, secondary.lon, 1e-8);
+        assertEquals(12, secondary.x);
+        assertEquals(14, secondary.y);
+        assertEquals(14012, secondary.time);
+    }
+
+    @Test
     public void testPointInPolygonTest_GeometriesCreatedByFactory() throws Exception {
         final Polygon polygon = factory.createPolygon(createPoints(new double[]{
-                2, 2,
-                6, 2,
-                6, 6,
-                2, 6,
-                2, 2
+                    2, 2,
+                    6, 2,
+                    6, 6,
+                    2, 6,
+                    2, 2
         }));
         assertTrue(polygon.contains(factory.createPoint(4, 4)));
     }
@@ -147,6 +180,7 @@ public class SampleCollectorTest {
     }
 
     private class TestTimeLocator implements TimeLocator {
+
         @Override
         public long getTimeFor(int x, int y) {
             return x + 1000 * y;
