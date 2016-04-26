7e56da438f4d9c241a46cb6cf924e2d981c91817
tom <tom.block@brockmann-consult.de>
2016-04-26 09:22:45 +0200

    added start and run scripts for matchup tool

---
+++ cems/src/main/bin/matchup_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+# example usage: matchup-run.sh 1982-234 1982-231 ./config_dir mms02.xml
+
+start_date=$1
+end_date=$2
+config_dir=$3
+use_case_config=$4
+
+echo "`date -u +%Y%m%d-%H%M%S` matchup $(use_case_config} ${start_date} - ${end_date} ..."
+
+${MMS_HOME}/bin/matchup-tool.sh -start ${start_date} -end ${end_date} -c ${config_dir} -u $(use_case_config}
---
+++ cems/src/main/bin/matchup_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ${MMS_HOME}/bin/mms-env.sh
+
+start_date=$1
+end_date=$2
+config_dir=$3
+use_case_config=$4
+
+task="matchup"
+jobname="${task}-${use_case_config}-${start_date}-${end_date}"
+echo ${jobname}
+
+command="${task}_run.sh ${start_date} ${end_date} ${config_dir} ${use_case_config}"
+
+echo "`date -u +%Y%m%d-%H%M%S` submitting job '${jobname}'"
+
+read_task_jobs ${jobname}
+
+if [ -z ${jobs} ]; then
+    submit_job ${jobname} ${command}
+fi
+
+wait_for_task_jobs_completion ${jobname}
