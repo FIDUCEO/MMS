5cce79e3f30bbc7eeb78bcf9d52c9395b2b8aa76
tom <tom.block@brockmann-consult.de>
2016-04-28 11:34:03 +0200

    corrected usecase and run script

--- cems/src/main/bin/matchup_run.sh
+++ cems/src/main/bin/matchup_run.sh
@@ -7,6 +7,6 @@ end_date=$2
 config_dir=$3
 use_case_config=$4
 
-echo "`date -u +%Y%m%d-%H%M%S` matchup $(use_case_config} ${start_date} - ${end_date} ..."
+echo "`date -u +%Y%m%d-%H%M%S` matchup ${use_case_config} ${start_date} - ${end_date} ..."
 
-${MMS_HOME}/bin/matchup-tool.sh -start ${start_date} -end ${end_date} -c ${config_dir} -u $(use_case_config}
+${MMS_HOME}/bin/matchup-tool.sh -start ${start_date} -end ${end_date} -c ${config_dir} -u ${use_case_config}
--- cems/src/main/python/usecase02_avhrr_n18_n17.py
+++ cems/src/main/python/usecase02_avhrr_n18_n17.py
@@ -4,6 +4,6 @@ w = Workflow('usecase02_avhrr_n18_n17', 7, '/group_workspaces/cems2/fiduceo/Soft
 w.add_primary_sensor('avhrr-n18', '2008-05-01', '2008-05-31', 'v01.2')
 w.add_secondary_sensor('avhrr-n17', '2008-05-01', '2008-05-31', 'v01.2')
 
-w.set_usecase_config("usecase-02.xml")
+w.set_usecase_config('usecase-02.xml')
 
 w.run_matchup(hosts=[('localhost', 4)])
