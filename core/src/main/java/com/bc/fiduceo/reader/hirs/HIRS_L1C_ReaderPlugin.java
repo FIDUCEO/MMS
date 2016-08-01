bba73ca2d7e37bca0fe774251c1ad281e06d566b
tom <tom.block@brockmann-consult.de>
2016-08-01 12:09:29 +0200

    implemented HIRS reader plugin and reader skeleton

---
+++ core/src/main/java/com/bc/fiduceo/reader/hirs/HIRS_L1C_ReaderPlugin.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 Brockmann Consult GmbH
+ * This code was developed for the EC project "Fidelity and Uncertainty in
+ * Climate Data Records from Earth Observations (FIDUCEO)".
+ * Grant Agreement: 638822
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * A copy of the GNU General Public License should have been supplied along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
+package com.bc.fiduceo.reader.hirs;
+
+import com.bc.fiduceo.reader.Reader;
+import com.bc.fiduceo.reader.ReaderPlugin;
+
+public class HIRS_L1C_ReaderPlugin implements ReaderPlugin {
+
+    private static final String[] SENSOR_KEYS = {"hirs-ma", "hirs-mb", "hirs-n06", "hirs-n07", "hirs-n08", "hirs-n09", "hirs-n10", "hirs-n11", "hirs-n12", "hirs-n14", "hirs-n15", "hirs-n16", "hirs-n17", "nirs-n18", "hirs-n19", "hirs-tn"};
+
+    @Override
+    public Reader createReader() {
+        return new HIRS_L1C_Reader();
+    }
+
+    @Override
+    public String[] getSupportedSensorKeys() {
+        return SENSOR_KEYS;
+    }
+}
