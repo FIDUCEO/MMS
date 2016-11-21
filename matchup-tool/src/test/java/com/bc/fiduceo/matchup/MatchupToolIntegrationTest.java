6f8daed3780db0f89e732c606dc8d39429d8bbd1
tom <tom.block@brockmann-consult.de>
2016-11-21 15:11:44 +0100

    updated version

--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolIntegrationTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolIntegrationTest.java
@@ -37,7 +37,7 @@ import static org.junit.Assert.assertEquals;
 public class MatchupToolIntegrationTest {
 
     private final String ls = System.lineSeparator();
-    private final String expectedPrintUsage = "matchup-tool version 1.1.0" + ls +
+    private final String expectedPrintUsage = "matchup-tool version 1.1.1-SNAPSHOT" + ls +
             ls +
             "usage: matchup-tool <options>" + ls +
             "Valid options are:" + ls +
