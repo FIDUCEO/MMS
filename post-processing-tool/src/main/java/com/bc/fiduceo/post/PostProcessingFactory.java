29c233b3d3f505a430ea858c9abd33845741eab3
sabine <sabine.embacher@brockmann-consult.de>
2016-11-25 16:54:03 +0100

    Post Processing ...

---
+++ post-processing-tool/src/main/java/com/bc/fiduceo/post/PostProcessingFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 Brockmann Consult GmbH
+ * This code was developed for the EC project "Fidelity and Uncertainty in
+ * Climate Data Records from Earth Observations (FIDUCEO)".
+ * Grant Agreement: 638822
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * A copy of the GNU General Public License should have been supplied along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ */
+
+package com.bc.fiduceo.post;
+
+import com.bc.ceres.core.ServiceRegistry;
+import com.bc.ceres.core.ServiceRegistryManager;
+import org.esa.snap.SnapCoreActivator;
+import org.jdom.Element;
+
+import java.util.HashMap;
+
+public class PostProcessingFactory {
+
+    private static PostProcessingFactory conditionFactory;
+    private final HashMap<String, PostProcessingPlugin> conditionPluginMap = new HashMap<>();
+
+    private PostProcessingFactory() {
+        final ServiceRegistryManager serviceRegistryManager = ServiceRegistryManager.getInstance();
+        final ServiceRegistry<PostProcessingPlugin> conditionPlugins = serviceRegistryManager.getServiceRegistry(PostProcessingPlugin.class);
+        SnapCoreActivator.loadServices(conditionPlugins);
+
+        for (PostProcessingPlugin plugin : conditionPlugins.getServices()) {
+            final String key = plugin.getPostProcessingName();
+            conditionPluginMap.put(key, plugin);
+        }
+    }
+
+    public static PostProcessingFactory get() {
+        if (conditionFactory == null) {
+            conditionFactory = new PostProcessingFactory();
+        }
+        return conditionFactory;
+    }
+
+    public PostProcessing getPostProcessing(Element element) {
+        final String name = element.getName();
+
+        final PostProcessingPlugin plugin = conditionPluginMap.get(name);
+        if (plugin == null) {
+            throw new IllegalArgumentException("Condition for name '" + name + "' not available.");
+        }
+        return plugin.createPostProcessing(element);
+    }
+}
