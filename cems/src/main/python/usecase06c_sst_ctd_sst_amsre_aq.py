799d322c1df6aeb1c087f74994ffaa94ee418fad
tom <tom.block@brockmann-consult.de>
2016-12-02 16:04:57 +0100

    added matchup scripts for SST usecase 6c

---
+++ cems/src/main/python/usecase06c_sst_ctd_sst_amsre_aq.py
@@ -0,0 +1,9 @@
+from workflow import Workflow
+
+w = Workflow('usecase06c_sst_ctd_sst_amsre_aq', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config')
+w.add_primary_sensor('ctd-sst', '2002-06-01', '2011-10-04', 'v03.3')
+w.add_secondary_sensor('amsre-aq', '2002-06-01', '2011-10-04', 'v12')
+
+w.set_usecase_config('usecase-06s-sst.xml')
+
+w.run_matchup(hosts=[('localhost', 24)])
