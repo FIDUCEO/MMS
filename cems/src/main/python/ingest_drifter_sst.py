27a84ff5408ab3e14045ef749e086da8c4aaa5bd
tom <tom.block@brockmann-consult.de>
2016-12-02 10:42:05 +0100

    removed insitu ingestion scripts

--- cems/src/main/python/ingest_drifter_sst.py
+++
@@ -1,6 +0,0 @@
-from workflow import Workflow
-
-w = Workflow('ingest_drifter_sst', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config')
-w.add_primary_sensor('drifter-sst', '1978-01-01', '2016-12-31', 'v03.3')
-
-w.run_ingestion(hosts=[('localhost', 1)])
