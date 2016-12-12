4fee6de19e851fdf4e0d4e015d7623cab4ead2b0
tom <tom.block@brockmann-consult.de>
2016-12-12 14:12:40 +0100

    new version 1.1.2

--- ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
+++ ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
@@ -55,7 +55,7 @@ public class IngestionToolTest {
 
         ingestionTool.printUsageTo(outputStream);
 
-        assertEquals("ingestion-tool version 1.1.2-SNAPSHOT" + ls +
+        assertEquals("ingestion-tool version 1.1.2" + ls +
                 ls +
                 "usage: ingestion-tool <options>" + ls +
                 "Valid options are:" + ls +
--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
@@ -78,7 +78,7 @@ public class MatchupToolTest {
 
         matchupTool.printUsageTo(outputStream);
 
-        assertEquals("matchup-tool version 1.1.2-SNAPSHOT" + ls +
+        assertEquals("matchup-tool version 1.1.2" + ls +
                      ls +
                      "usage: matchup-tool <options>" + ls +
                      "Valid options are:" + ls +
