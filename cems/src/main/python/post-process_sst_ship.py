fb050acc72ae073cc78eb8b707464e1f0b116ebe
tom <tom.block@brockmann-consult.de>
2017-01-17 13:45:06 +0100

    added workflows for mmd6c post-processing

---
+++ cems/src/main/python/post-process_sst_ship.py
@@ -0,0 +1,10 @@
+from period import Period
+from workflow import Workflow
+
+period = Period('2002-06-01', '2011-10-07')
+w = Workflow('post_process_sst_ship', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config')
+
+w.set_input_dir('/group_workspaces/cems2/esacci_sst/mms_new/mmd/mmd06c/ship-sst_amsre-aq')
+w.set_usecase_config('usecase-06s-pp.xml')
+
+w.run_post_processing(hosts=[('localhost', 24)])
