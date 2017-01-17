b4d45bdad8c6f2723d6413c8a46f7563fcefaef7
tom <tom.block@brockmann-consult.de>
2017-01-17 15:20:25 +0100

    corrected workflows

--- cems/src/main/python/post-process_sst_animal.py
+++ cems/src/main/python/post-process_sst_animal.py
@@ -2,7 +2,7 @@ from period import Period
 from workflow import Workflow
 
 period = Period('2004-01-08', '2011-10-07')
-w = Workflow('post_process_sst_animal', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config')
+w = Workflow('post_process_sst_animal', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config', period)
 
 w.set_input_dir('/group_workspaces/cems2/esacci_sst/mms_new/mmd/mmd06c/animal-sst_amsre-aq')
 w.set_usecase_config('usecase-06-pp.xml')
