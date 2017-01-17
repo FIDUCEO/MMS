afbceae5c7c931e9e905049e2b3d6bb7d25e890d
tom <tom.block@brockmann-consult.de>
2017-01-17 14:21:03 +0100

    added post-procesing config, updated workflows and installation scripts

--- cems/src/main/python/post-process_sst_bottle.py
+++ cems/src/main/python/post-process_sst_bottle.py
@@ -5,6 +5,6 @@ period = Period('2002-06-01', '2011-10-07')
 w = Workflow('post_process_sst_bottle', 7, '/group_workspaces/cems2/esacci_sst/mms_new/config')
 
 w.set_input_dir('/group_workspaces/cems2/esacci_sst/mms_new/mmd/mmd06c/bottle-sst_amsre-aq')
-w.set_usecase_config('usecase-06s-pp.xml')
+w.set_usecase_config('usecase-06-pp.xml')
 
 w.run_post_processing(hosts=[('localhost', 24)])
