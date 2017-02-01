dce4932f7dca8ef2182f6a108ffa5294cbc12606
tom <tom.block@brockmann-consult.de>
2017-02-01 17:07:21 +0100

    added test

--- post-processing-tool/src/test/java/com/bc/fiduceo/post/plugin/nwp/NwpUtilsTest.java
+++ post-processing-tool/src/test/java/com/bc/fiduceo/post/plugin/nwp/NwpUtilsTest.java
@@ -1,3 +1,24 @@
+
+/*
+ * Copyright (C) 2017 Brockmann Consult GmbH
+ * This code was developed for the EC project "Fidelity and Uncertainty in
+ * Climate Data Records from Earth Observations (FIDUCEO)".
+ * Grant Agreement: 638822
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 3 of the License, or (at your option)
+ *  any later version.
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  more details.
+ *
+ *  A copy of the GNU General Public License should have been supplied along
+ *  with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
 package com.bc.fiduceo.post.plugin.nwp;
 
 import org.junit.Test;
@@ -10,5 +31,11 @@ public class NwpUtilsTest {
     public void testComputeFutureTimeStepCount() {
         assertEquals(12, NwpUtils.computeFutureTimeStepCount(33));
         assertEquals(6, NwpUtils.computeFutureTimeStepCount(17));
+    }
+
+    @Test
+    public void testComputePastTimeStepCount() throws Exception {
+        assertEquals(20, NwpUtils.computePastTimeStepCount(33));
+        assertEquals(10, NwpUtils.computePastTimeStepCount(17));
     }
 }
