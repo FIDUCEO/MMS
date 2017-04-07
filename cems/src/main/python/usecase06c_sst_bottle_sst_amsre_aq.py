28de471656ea9fb229e006c97386f903b8aa8673
tom <tom.block@brockmann-consult.de>
2017-04-07 11:06:27 +0200

    corrected MMd6c workflows

--- cems/src/main/python/usecase06c_sst_bottle_sst_amsre_aq.py
+++ cems/src/main/python/usecase06c_sst_bottle_sst_amsre_aq.py
@@ -4,6 +4,6 @@ w = Workflow('usecase06c_sst_bottle_sst_amsre_aq', 7, '/group_workspaces/cems2/e
 w.add_primary_sensor('bottle-sst', '2002-06-01', '2011-10-04', 'v03.3')
 w.add_secondary_sensor('amsre-aq', '2002-06-01', '2011-10-04', 'v12')
 
-w.set_usecase_config('usecase-06s-sst.xml')
+w.set_usecase_config('usecase-06-sst.xml')
 
 w.run_matchup(hosts=[('localhost', 24)])
