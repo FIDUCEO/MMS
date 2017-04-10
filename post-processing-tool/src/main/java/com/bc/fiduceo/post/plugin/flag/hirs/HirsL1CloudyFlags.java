8312b3860eed9934e5a71cf216f7e764b5aa2afe
sabine <sabine.embacher@brockmann-consult.de>
2017-04-10 17:02:43 +0200

    Implement post processing ... cloudy flag generation for HIRS

--- post-processing-tool/src/main/java/com/bc/fiduceo/post/plugin/flag/hirs/HirsL1CloudyFlags.java
+++ post-processing-tool/src/main/java/com/bc/fiduceo/post/plugin/flag/hirs/HirsL1CloudyFlags.java
@@ -18,15 +18,24 @@ package com.bc.fiduceo.post.plugin.flag.hirs;
 
 import com.bc.fiduceo.post.PostProcessing;
 import com.bc.fiduceo.util.NetCDFUtils;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayByte;
 import ucar.ma2.DataType;
 import ucar.ma2.InvalidRangeException;
+import ucar.nc2.Attribute;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFileWriter;
 import ucar.nc2.Variable;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 class HirsL1CloudyFlags extends PostProcessing {
+
+    public final static byte SPACE_CONTRAST_TEST_ALL_PIXELS_USABLE = 1;
+    public final static byte SPACE_CONTRAST_TEST_WARNING = 2;
+    public final static byte SPACE_CONTRAST_TEST_CLOUDY = 4;
+    public final static byte INTERCHANNEL_TEST_CLOUDY = 8;
 
     private final static DataType FLAG_VAR_DATA_TYPE = DataType.BYTE;
 
@@ -46,6 +55,25 @@ class HirsL1CloudyFlags extends PostProcessing {
         final String dimensions = variable.getDimensionsString();
         final Variable flagVar = writer.addVariable(null, flagVarName, FLAG_VAR_DATA_TYPE, dimensions);
         // todo add flag meanings and other Attributes to flagVar
+        flagVar.addAttribute(new Attribute("flag_meanings", Arrays.asList("sc_all",
+                                                                          "sc_warning",
+                                                                          "sc_cloudy",
+                                                                          "ic_cloudy")));
+        Array masks = new ArrayByte(new int[]{4});
+        masks.setByte(0, SPACE_CONTRAST_TEST_ALL_PIXELS_USABLE);
+        masks.setByte(1, SPACE_CONTRAST_TEST_WARNING);
+        masks.setByte(2, SPACE_CONTRAST_TEST_CLOUDY);
+        masks.setByte(3, INTERCHANNEL_TEST_CLOUDY);
+        flagVar.addAttribute(new Attribute("flag_masks", masks));
+        flagVar.addAttribute(new Attribute("flag_coding_name", "hirs_cloudy_flags"));
+        final String Separator = "\t";
+        flagVar.addAttribute(new Attribute("flag_descriptions", "space contrast test, all pixels are usable"
+                                                                + Separator +
+                                                                "space contrast test, warning, less than 99 percent are usable"
+                                                                + Separator +
+                                                                "space contrast test, cloudy"
+                                                                + Separator +
+                                                                "interchannel test, cloudy"));
     }
 
     @Override
--- post-processing-tool/src/test/java/com/bc/fiduceo/post/plugin/flag/hirs/HirsL1CloudyFlagsTest.java
+++ post-processing-tool/src/test/java/com/bc/fiduceo/post/plugin/flag/hirs/HirsL1CloudyFlagsTest.java
@@ -1,15 +1,24 @@
 package com.bc.fiduceo.post.plugin.flag.hirs;
 
+import static com.bc.fiduceo.post.plugin.flag.hirs.HirsL1CloudyFlags.INTERCHANNEL_TEST_CLOUDY;
+import static com.bc.fiduceo.post.plugin.flag.hirs.HirsL1CloudyFlags.SPACE_CONTRAST_TEST_ALL_PIXELS_USABLE;
+import static com.bc.fiduceo.post.plugin.flag.hirs.HirsL1CloudyFlags.SPACE_CONTRAST_TEST_CLOUDY;
+import static com.bc.fiduceo.post.plugin.flag.hirs.HirsL1CloudyFlags.SPACE_CONTRAST_TEST_WARNING;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import com.bc.fiduceo.post.PostProcessing;
 import org.junit.*;
+import ucar.ma2.Array;
+import ucar.ma2.ArrayByte;
 import ucar.ma2.DataType;
+import ucar.nc2.Attribute;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.NetcdfFileWriter;
 import ucar.nc2.Variable;
+
+import java.util.Arrays;
 
 public class HirsL1CloudyFlagsTest {
 
@@ -50,12 +59,33 @@ public class HirsL1CloudyFlagsTest {
 
         when(file.findVariable(null, btVarName_11_1_µm)).thenReturn(variable);
         when(variable.getDimensionsString()).thenReturn("a b c");
+        when(fileWriter.addVariable(null, flagVarName, DataType.BYTE, "a b c")).thenReturn(variable);
 
         postProcessing.prepare(file, fileWriter);
 
         verify(file, times(1)).findVariable(null, btVarName_11_1_µm);
         verify(variable, times(1)).getDimensionsString();
         verify(fileWriter, times(1)).addVariable(null, flagVarName, DataType.BYTE, "a b c");
+        verify(variable, times(1)).addAttribute(new Attribute("flag_meanings", Arrays.asList("sc_all",
+                                                                                             "sc_warning",
+                                                                                             "sc_cloudy",
+                                                                                             "ic_cloudy")));
+
+        Array masks = new ArrayByte(new int[]{4});
+        masks.setByte(0, SPACE_CONTRAST_TEST_ALL_PIXELS_USABLE);
+        masks.setByte(1, SPACE_CONTRAST_TEST_WARNING);
+        masks.setByte(2, SPACE_CONTRAST_TEST_CLOUDY);
+        masks.setByte(3, INTERCHANNEL_TEST_CLOUDY);
+        verify(variable, times(1)).addAttribute(new Attribute("flag_masks", masks));
+        verify(variable, times(1)).addAttribute(new Attribute("flag_coding_name", "hirs_cloudy_flags"));
+        final String Separator = "\t";
+        verify(variable, times(1)).addAttribute(new Attribute("flag_descriptions", "space contrast test, all pixels are usable"
+                                                                        + Separator +
+                                                                        "space contrast test, warning, less than 99 percent are usable"
+                                                                        + Separator +
+                                                                        "space contrast test, cloudy"
+                                                                        + Separator +
+                                                                        "interchannel test, cloudy"));
         verifyNoMoreInteractions(file, variable, fileWriter);
     }
 }
