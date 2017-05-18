975bc24b196f76fab4e57bdcb5651f728c114928
tom <tom.block@brockmann-consult.de>
2017-05-18 16:36:27 +0200

    started integration test

---
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolIntegrationTest_useCase_03.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 Brockmann Consult GmbH
+ * This code was developed for the EC project "Fidelity and Uncertainty in
+ * Climate Data Records from Earth Observations (FIDUCEO)".
+ * Grant Agreement: 638822
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * A copy of the GNU General Public License should have been supplied along
+ * with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
+package com.bc.fiduceo.matchup;
+
+import com.bc.fiduceo.NCTestUtils;
+import com.bc.fiduceo.TestUtil;
+import com.bc.fiduceo.core.Dimension;
+import com.bc.fiduceo.core.SatelliteObservation;
+import com.bc.fiduceo.core.Sensor;
+import com.bc.fiduceo.core.UseCaseConfig;
+import com.bc.fiduceo.db.DbAndIOTestRunner;
+import org.apache.commons.cli.ParseException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import ucar.ma2.InvalidRangeException;
+import ucar.nc2.NetcdfFile;
+import ucar.nc2.iosp.netcdf3.N3iosp;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@SuppressWarnings("ThrowFromFinallyBlock")
+@RunWith(DbAndIOTestRunner.class)
+public class MatchupToolIntegrationTest_useCase_03 extends AbstractUsecaseIntegrationTest {
+
+    @Test
+    public void testMatchup_overlappingSensingTimes() throws IOException, ParseException, SQLException, InvalidRangeException {
+        // @todo 1 tb/tb implement 2017-05-18
+    }
+}
