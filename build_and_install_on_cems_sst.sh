5bb7f41ef3a728e85cc64b06710f2fe9937d61a4
tom <tom.block@brockmann-consult.de>
2017-06-20 09:48:32 +0200

    updated build scripts

--- build_and_install_on_cems_sst.sh
+++ build_and_install_on_cems_sst.sh
@@ -2,4 +2,4 @@ git pull github master
 mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
-cp -r target/fiduceo-master-1.2.2-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
+cp -r target/fiduceo-master-1.2.3-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
