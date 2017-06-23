0a4c6d5bd8124e60481a6104552426cfaf4d458c
tom <tom.block@brockmann-consult.de>
2017-06-23 11:36:00 +0200

    removed snapshot

--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
@@ -83,7 +83,7 @@ public class MatchupToolTest {
 
         matchupTool.printUsageTo(outputStream);
 
-        assertEquals("matchup-tool version 1.2.4-SNAPSHOT" + ls +
+        assertEquals("matchup-tool version 1.2.4" + ls +
                 ls +
                 "usage: matchup-tool <options>" + ls +
                 "Valid options are:" + ls +
