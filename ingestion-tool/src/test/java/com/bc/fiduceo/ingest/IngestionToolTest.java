b9f821ec3c4e2b68a845bdd78a96ccbabf66bda4
tom <tom.block@brockmann-consult.de>
2017-07-25 12:30:12 +0200

    new snapshot version

--- ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
+++ ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
@@ -59,7 +59,7 @@ public class IngestionToolTest {
 
         ingestionTool.printUsageTo(outputStream);
 
-        assertEquals("ingestion-tool version 1.2.6" + ls +
+        assertEquals("ingestion-tool version 1.2.7-SNAPSHOT" + ls +
                 ls +
                 "usage: ingestion-tool <options>" + ls +
                 "Valid options are:" + ls +
