12eda8d26a7552ea0eedfcbe28899f6ab5f137cc
sabine <sabine.embacher@brockmann-consult.de>
2017-12-13 12:01:56 +0100

    Set version to 1.3.5 update Tests

--- ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
+++ ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
@@ -59,7 +59,7 @@ public class IngestionToolTest {
 
         ingestionTool.printUsageTo(outputStream);
 
-        assertEquals("ingestion-tool version 1.3.5-SNAPSHOT" + ls +
+        assertEquals("ingestion-tool version 1.3.5" + ls +
                 ls +
                 "usage: ingestion-tool <options>" + ls +
                 "Valid options are:" + ls +
