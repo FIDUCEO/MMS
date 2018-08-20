8ba44ea7d4fe7331c306266df2bac1433e7d5bd2
sabine <sabine.embacher@brockmann-consult.de>
2018-08-20 14:32:27 +0200

    install scripts

--- build_and_install_on_cems.sh
+++ build_and_install_on_cems.sh
@@ -3,4 +3,4 @@ git pull github master
 mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/fiduceo/Software/mms/bin/*
-cp -r target/fiduceo-master-1.3.4-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
+cp -r target/fiduceo-master-1.4.0-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
--- build_and_install_on_cems_sst.sh
+++ build_and_install_on_cems_sst.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 git pull github master
 mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
-cp -r target/fiduceo-master-1.3.4-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
+cp -r target/fiduceo-master-1.4.0-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
