93389f6609f30cc04a55c912db6e069296645447
sabine <sabine.embacher@brockmann-consult.de>
2018-09-05 15:02:37 +0200

    add SNAPSHOT to bash scripts

--- build_and_install_on_cems_sst.sh
+++ build_and_install_on_cems_sst.sh
@@ -6,5 +6,5 @@ bash backup_bin_to_bin_archive_with_timestamp_sst.sh
 echo "clean up bin dir"
 rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
 echo "copy build result to bin dir"
-cp -r target/fiduceo-master-1.4.1-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
+cp -r target/fiduceo-master-1.4.1-SNAPSHOT-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
 
