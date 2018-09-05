530507a1b01eb3b41876677b8acc46063c3d926b
sabine <sabine.embacher@brockmann-consult.de>
2018-09-05 11:31:30 +0200

    add linespace

--- build_and_install_on_cems.sh
+++ build_and_install_on_cems.sh
@@ -4,3 +4,4 @@ mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/fiduceo/Software/mms/bin/*
 cp -r target/fiduceo-master-1.4.1-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
+
--- build_and_install_on_cems_sst.sh
+++ build_and_install_on_cems_sst.sh
@@ -4,3 +4,4 @@ mvn clean install package assembly:directory
 
 rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
 cp -r target/fiduceo-master-1.4.1-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
+
