597ad81306ca7456e8c2bb97e96c4a7d21106487
sabine <sabine.embacher@brockmann-consult.de>
2018-09-05 16:34:51 +0200

    add preserve bin dir via move (mv)

--- backup_bin_to_bin_archive_with_timestamp.sh
+++ backup_bin_to_bin_archive_with_timestamp.sh
@@ -2,7 +2,7 @@
 mmspath="/group_workspaces/cems2/fiduceo/Software/mms"
 binpath=$mmspath/bin
 binarcpath=$mmspath/bin_archive
-bintime="$(find "$binpath" -name lib -type d -printf "%CF_%CT %f\n" | cut -c -19)"
+bintime="$(find "$binpath" -name lib -type d -printf "%TF_%TT %f\n" | cut -c -19)"
 echo "backup bin dir to bin_archive with timestamp $bintime"
-cp -a "$binpath" "$binarcpath/${binpath##*/}-$bintime"
+mv "$binpath" "$binarcpath/${binpath##*/}-$bintime"
 echo "... done"
--- backup_bin_to_bin_archive_with_timestamp_sst.sh
+++ backup_bin_to_bin_archive_with_timestamp_sst.sh
@@ -2,7 +2,7 @@
 mmspath="/group_workspaces/cems2/esacci_sst/mms_new"
 binpath=$mmspath/bin
 binarcpath=$mmspath/bin_archive
-bintime="$(find "$binpath" -name lib -type d -printf "%CF_%CT %f\n" | cut -c -19)"
+bintime="$(find "$binpath" -name lib -type d -printf "%TF_%TT %f\n" | cut -c -19)"
 echo "backup bin dir to bin_archive with timestamp $bintime"
-cp -a "$binpath" "$binarcpath/${binpath##*/}-$bintime"
+mv "$binpath" "$binarcpath/${binpath##*/}-$bintime"
 echo "... done"
