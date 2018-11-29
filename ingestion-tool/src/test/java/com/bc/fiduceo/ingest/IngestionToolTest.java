2d8d83ccbbc1908ef42c19c1dc203e28b6dabe6c
tom <tom.block@brockmann-consult.de>
2018-11-29 10:42:45 +0100

    new version number

--- ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
+++ ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
@@ -59,7 +59,7 @@ public class IngestionToolTest {
 
         ingestionTool.printUsageTo(outputStream);
 
-        assertEquals("ingestion-tool version 1.4.1-SNAPSHOT" + ls +
+        assertEquals("ingestion-tool version 1.4.1" + ls +
                 ls +
                 "usage: ingestion-tool <options>" + ls +
                 "Valid options are:" + ls +
