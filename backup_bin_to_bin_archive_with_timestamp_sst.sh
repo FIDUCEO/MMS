94e43563a69d00f4258dfd5f8ffad8e126b82748
tom <tom.block@brockmann-consult.de>
2018-11-30 08:11:28 +0100

    corrected build scripts

--- backup_bin_to_bin_archive_with_timestamp_sst.sh
+++ backup_bin_to_bin_archive_with_timestamp_sst.sh
@@ -4,5 +4,5 @@ binpath=$mmspath/bin
 binarcpath=$mmspath/bin_archive
 bintime="$(find "$binpath" -name lib -type d -printf "%TF_%TT %f\n" | cut -c -19)"
 echo "backup bin dir to bin_archive with timestamp $bintime"
-mv "$binpath" "$binarcpath/${binpath##*/}-$bintime"
+cp "$binpath" "$binarcpath/${binpath##*/}-$bintime"
 echo "... done"
--- build_and_install_on_cems_sst.sh
+++ build_and_install_on_cems_sst.sh
@@ -4,7 +4,7 @@ mvn clean install package assembly:directory
 
 bash backup_bin_to_bin_archive_with_timestamp_sst.sh
 # echo "clean up bin dir"
-# rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
 echo "copy build result to bin dir"
+rm -rf /group_workspaces/cems2/esacci_sst/mms_new/bin/*
 cp -a target/fiduceo-master-1.4.1-MMS/* /group_workspaces/cems2/esacci_sst/mms_new/bin
 
