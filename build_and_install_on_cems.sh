5eb5ed921bd4a51a9e3a9af052163c8b1633347c
tom <tom.block@brockmann-consult.de>
2018-11-30 08:07:32 +0100

    corrected buils scripts

--- build_and_install_on_cems.sh
+++ build_and_install_on_cems.sh
@@ -6,5 +6,5 @@ bash backup_bin_to_bin_archive_with_timestamp.sh
 # echo "clean up bin dir"
 # rm -rf /group_workspaces/cems2/fiduceo/Software/mms/bin/*
 echo "copy build result to bin dir"
-cp -a target/fiduceo-master-1.4.1-SNAPSHOT-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
+cp -a target/fiduceo-master-1.4.1-MMS/* /group_workspaces/cems2/fiduceo/Software/mms/bin
 
