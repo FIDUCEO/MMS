324a885228ac2f618645ee66d5c1770ff7b397b7
tom <tom.block@brockmann-consult.de>
2018-12-03 11:28:41 +0100

    new version number

--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
@@ -83,7 +83,7 @@ public class MatchupToolTest {
 
         matchupTool.printUsageTo(outputStream);
 
-        assertEquals("matchup-tool version 1.4.1" + ls +
+        assertEquals("matchup-tool version 1.4.2-SNAPSHOT" + ls +
                 ls +
                 "usage: matchup-tool <options>" + ls +
                 "Valid options are:" + ls +
