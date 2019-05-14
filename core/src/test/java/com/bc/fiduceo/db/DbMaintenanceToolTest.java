b2c5354df58db1fdc91e8095f37eabef02761329
tom <tom.block@brockmann-consult.de>
2019-05-14 16:28:40 +0200

    version number

--- core/src/test/java/com/bc/fiduceo/db/DbMaintenanceToolTest.java
+++ core/src/test/java/com/bc/fiduceo/db/DbMaintenanceToolTest.java
@@ -52,7 +52,7 @@ public class DbMaintenanceToolTest {
 
         tool.printUsageTo(stream);
 
-        assertEquals("db-maintenance-tool version 1.4.5-SNAPSHOT" + ls +
+        assertEquals("db-maintenance-tool version 1.4.5" + ls +
                 ls +
                 "usage: db-maintenance-tool <options>" + ls +
                 "Valid options are:" + ls +
--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
@@ -84,7 +84,7 @@ public class MatchupToolTest {
 
         matchupTool.printUsageTo(outputStream);
 
-        assertEquals("matchup-tool version 1.4.5-SNAPSHOT" + ls +
+        assertEquals("matchup-tool version 1.4.5" + ls +
                 ls +
                 "usage: matchup-tool <options>" + ls +
                 "Valid options are:" + ls +
