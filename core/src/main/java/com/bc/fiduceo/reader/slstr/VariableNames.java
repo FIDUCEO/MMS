c89701b5ae56e3ffb0e022e47de0d11cfe123629
TomBlock <tom.block@brockmann-consult.de>
2019-05-20 15:13:09 +0200

    finished VariableNames class

--- core/src/main/java/com/bc/fiduceo/reader/slstr/VariableNames.java
+++ core/src/main/java/com/bc/fiduceo/reader/slstr/VariableNames.java
@@ -3,67 +3,94 @@ package com.bc.fiduceo.reader.slstr;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.bc.fiduceo.reader.slstr.VariableType.*;
+
 class VariableNames {
 
-    private final List<String> variableNames;
+    private final List<String> nadir1kmNames;
+    private final List<String> nadir500mNames;
+    private final List<String> oblique1kmNames;
+    private final List<String> oblique500mNames;
 
     VariableNames() {
-        variableNames = new ArrayList<>();
-        variableNames.add("latitude_tx");
-        variableNames.add("longitude_tx");
-        variableNames.add("sat_azimuth_tn");
-        variableNames.add("sat_zenith_tn");
-        variableNames.add("solar_azimuth_tn");
-        variableNames.add("solar_zenith_tn");
-        variableNames.add("S7_BT_in");
-        variableNames.add("S8_BT_in");
-        variableNames.add("S9_BT_in");
-        variableNames.add("S7_exception_in");
-        variableNames.add("S8_exception_in");
-        variableNames.add("S9_exception_in");
-        variableNames.add("S1_radiance_an");
-        variableNames.add("S2_radiance_an");
-        variableNames.add("S3_radiance_an");
-        variableNames.add("S4_radiance_an");
-        variableNames.add("S5_radiance_an");
-        variableNames.add("S6_radiance_an");
-        variableNames.add("S1_exception_an");
-        variableNames.add("S2_exception_an");
-        variableNames.add("S3_exception_an");
-        variableNames.add("S4_exception_an");
-        variableNames.add("S5_exception_an");
-        variableNames.add("S6_exception_an");
-        variableNames.add("sat_azimuth_to");
-        variableNames.add("sat_zenith_to");
-        variableNames.add("solar_azimuth_to");
-        variableNames.add("solar_zenith_to");
-        variableNames.add("S7_BT_io");
-        variableNames.add("S8_BT_io");
-        variableNames.add("S9_BT_io");
-        variableNames.add("S7_exception_io");
-        variableNames.add("S8_exception_io");
-        variableNames.add("S9_exception_io");
-        variableNames.add("S1_radiance_ao");
-        variableNames.add("S2_radiance_ao");
-        variableNames.add("S3_radiance_ao");
-        variableNames.add("S4_radiance_ao");
-        variableNames.add("S5_radiance_ao");
-        variableNames.add("S6_radiance_ao");
-        variableNames.add("S1_exception_ao");
-        variableNames.add("S2_exception_ao");
-        variableNames.add("S3_exception_ao");
-        variableNames.add("S4_exception_ao");
-        variableNames.add("S5_exception_ao");
-        variableNames.add("S6_exception_ao");
-        variableNames.add("confidence_in");
-        variableNames.add("pointing_in");
-        variableNames.add("bayes_in");
-        variableNames.add("cloud_in");
-        variableNames.add("bayes_io");
-        variableNames.add("cloud_io");
+        nadir500mNames = new ArrayList<>();
+        nadir500mNames.add("latitude_tx");
+        nadir500mNames.add("longitude_tx");
+        nadir500mNames.add("sat_azimuth_tn");
+        nadir500mNames.add("sat_zenith_tn");
+        nadir500mNames.add("solar_azimuth_tn");
+        nadir500mNames.add("solar_zenith_tn");
+        nadir500mNames.add("S1_radiance_an");
+        nadir500mNames.add("S2_radiance_an");
+        nadir500mNames.add("S3_radiance_an");
+        nadir500mNames.add("S4_radiance_an");
+        nadir500mNames.add("S5_radiance_an");
+        nadir500mNames.add("S6_radiance_an");
+        nadir500mNames.add("S1_exception_an");
+        nadir500mNames.add("S2_exception_an");
+        nadir500mNames.add("S3_exception_an");
+        nadir500mNames.add("S4_exception_an");
+        nadir500mNames.add("S5_exception_an");
+        nadir500mNames.add("S6_exception_an");
+
+        nadir1kmNames = new ArrayList<>();
+        nadir1kmNames.add("S7_BT_in");
+        nadir1kmNames.add("S8_BT_in");
+        nadir1kmNames.add("S9_BT_in");
+        nadir1kmNames.add("S7_exception_in");
+        nadir1kmNames.add("S8_exception_in");
+        nadir1kmNames.add("S9_exception_in");
+        nadir1kmNames.add("confidence_in");
+        nadir1kmNames.add("pointing_in");
+        nadir1kmNames.add("bayes_in");
+        nadir1kmNames.add("cloud_in");
+
+        oblique500mNames = new ArrayList<>();
+        oblique500mNames.add("sat_azimuth_to");
+        oblique500mNames.add("sat_zenith_to");
+        oblique500mNames.add("solar_azimuth_to");
+        oblique500mNames.add("solar_zenith_to");
+        oblique500mNames.add("S1_radiance_ao");
+        oblique500mNames.add("S2_radiance_ao");
+        oblique500mNames.add("S3_radiance_ao");
+        oblique500mNames.add("S4_radiance_ao");
+        oblique500mNames.add("S5_radiance_ao");
+        oblique500mNames.add("S6_radiance_ao");
+        oblique500mNames.add("S1_exception_ao");
+        oblique500mNames.add("S2_exception_ao");
+        oblique500mNames.add("S3_exception_ao");
+        oblique500mNames.add("S4_exception_ao");
+        oblique500mNames.add("S5_exception_ao");
+        oblique500mNames.add("S6_exception_ao");
+
+        oblique1kmNames = new ArrayList<>();
+        oblique1kmNames.add("S7_BT_io");
+        oblique1kmNames.add("S8_BT_io");
+        oblique1kmNames.add("S9_BT_io");
+        oblique1kmNames.add("S7_exception_io");
+        oblique1kmNames.add("S8_exception_io");
+        oblique1kmNames.add("S9_exception_io");
+        oblique1kmNames.add("bayes_io");
+        oblique1kmNames.add("cloud_io");
     }
 
     boolean isValidName(String variableName) {
-        return variableNames.contains(variableName);
+        return nadir500mNames.contains(variableName) ||
+                nadir1kmNames.contains(variableName) ||
+                oblique500mNames.contains(variableName) ||
+                oblique1kmNames.contains(variableName);
+    }
+
+    VariableType getVariableType(String variableName) {
+        if (nadir500mNames.contains(variableName)) {
+            return NADIR_500m;
+        } else if (nadir1kmNames.contains(variableName)) {
+            return NADIR_1km;
+        } else if (oblique500mNames.contains(variableName)) {
+            return OBLIQUE_500m;
+        } else if (oblique1kmNames.contains(variableName)) {
+            return OBLIQUE_1km;
+        }
+        throw new RuntimeException("Requested variable not supported: " + variableName);
     }
 }
--- core/src/test/java/com/bc/fiduceo/reader/slstr/VariableNamesTest.java
+++ core/src/test/java/com/bc/fiduceo/reader/slstr/VariableNamesTest.java
@@ -2,8 +2,8 @@ package com.bc.fiduceo.reader.slstr;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static com.bc.fiduceo.reader.slstr.VariableType.*;
+import static org.junit.Assert.*;
 
 public class VariableNamesTest {
 
@@ -20,6 +20,17 @@ public class VariableNamesTest {
 
     @Test
     public void testGetVariableType() {
+        final VariableNames variableNames = new VariableNames();
+        assertEquals(NADIR_500m, variableNames.getVariableType("longitude_tx"));
+        assertEquals(NADIR_500m, variableNames.getVariableType("solar_azimuth_tn"));
 
+        assertEquals(NADIR_1km, variableNames.getVariableType("S7_BT_in"));
+        assertEquals(NADIR_1km, variableNames.getVariableType("S9_exception_in"));
+
+        assertEquals(OBLIQUE_500m, variableNames.getVariableType("solar_zenith_to"));
+        assertEquals(OBLIQUE_500m, variableNames.getVariableType("S4_exception_ao"));
+
+        assertEquals(OBLIQUE_1km, variableNames.getVariableType("S9_BT_io"));
+        assertEquals(OBLIQUE_1km, variableNames.getVariableType("bayes_io"));
     }
 }
