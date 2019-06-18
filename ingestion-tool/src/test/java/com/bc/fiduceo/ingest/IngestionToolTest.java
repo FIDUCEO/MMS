a6e987926ded04df7dd2f5802159c45986cd1b36
tom <tom.block@brockmann-consult.de>
2019-06-18 18:20:09 +0200

    snapshot version

--- ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
+++ ingestion-tool/src/test/java/com/bc/fiduceo/ingest/IngestionToolTest.java
@@ -59,7 +59,7 @@ public class IngestionToolTest {
 
         ingestionTool.printUsageTo(outputStream);
 
-        assertEquals("ingestion-tool version 1.4.7" + ls +
+        assertEquals("ingestion-tool version 1.4.8-SNAPSHOT" + ls +
                 ls +
                 "usage: ingestion-tool <options>" + ls +
                 "Valid options are:" + ls +
--- matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
+++ matchup-tool/src/test/java/com/bc/fiduceo/matchup/MatchupToolTest.java
@@ -84,7 +84,7 @@ public class MatchupToolTest {
 
         matchupTool.printUsageTo(outputStream);
 
-        assertEquals("matchup-tool version 1.4.7" + ls +
+        assertEquals("matchup-tool version 1.4.8-SNAPSHOT" + ls +
                 ls +
                 "usage: matchup-tool <options>" + ls +
                 "Valid options are:" + ls +
