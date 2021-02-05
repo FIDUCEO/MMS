378ebdb763d0e6210f5c3d7709a7e3c8fa63053d
tom <tom.block@brockmann-consult.de>
2021-02-05 10:48:54 +0100

    changed input file regexp

--- post-processing-tool/src/test/java/com/bc/fiduceo/post/PostProcessingToolTest.java
+++ post-processing-tool/src/test/java/com/bc/fiduceo/post/PostProcessingToolTest.java
@@ -42,6 +42,7 @@ import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.hamcrest.Matchers.equalToIgnoringWhiteSpace;
 import static org.junit.Assert.assertEquals;
@@ -349,6 +350,18 @@ public class PostProcessingToolTest {
         assertTrue(namesList.contains("Karl-Heinz"));
         assertTrue(namesList.contains("Donald"));
         assertTrue(namesList.contains("Hermann"));
+    }
+
+    @Test
+    public void testFileNamePattern() {
+        final Pattern pattern = PostProcessingTool.getFileNamePattern();
+
+        assertTrue(pattern.matcher("mmd08_aatsr-en_avhrr-frac-ma_2012-092_2012-092.nc").matches());
+        assertTrue(pattern.matcher("mmd09_slstr-s3a-nt_avhrr-frac-ma_2019-115_2019-115.nc").matches());
+        assertTrue(pattern.matcher("coo_2_slstr-s3a-nt_avhrr-frac-mb_2020-241_2020-241.nc").matches());
+
+        assertFalse(pattern.matcher("NSS.FRAC.M3.D19261.S1708.E1849.B0448586.SV").matches());
+        assertFalse(pattern.matcher("20161122200700-ESACCI-L1C-AVHRR18_G-fv01.0.nc").matches());
     }
 
     private PostProcessingConfig getConfig() throws Exception {
